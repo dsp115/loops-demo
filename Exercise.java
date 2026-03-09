e9f798e674704f30a5f11e419d1c3f9ecbe016b0
dsp115 <dpinale1@my.epcc.edu>
2026-03-09 20:38:51 +0000

    -1

--- Exercise.java
+++ Exercise.java
@@ -16,8 +16,7 @@ public class Exercise
             score = keyboard.nextInt();
              
         }
-            
-        while (score < 0 || score > 100);
+            while (score < 0 || score > 100);
         
         if (score >= 90 && score <= 100) {
             System.out.println("A range");
@@ -25,7 +24,23 @@ public class Exercise
             System.out.println("B range");
         } else if (score < 80) {
             System.out.println("below B range");
+        } 
+
+
+
+        System.out.print("Enter number (-1 to stop): ");
+
+        int number = keyboard.nextInt();
+
+        int sum = 0;
+
+        while(number != -1) {
+            sum += number;
+
+            System.out.println("Enter number (-1 to stop): ");
+            number keyboard.nextInt();
         }
+        System.out.println("The sum is " + sum);
         
     }
 }
